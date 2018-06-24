f42c757b3f48a3fdb894af925408f0ef9efdb259
siemebipsbro <bpa@siemseverest.com>
2018-06-25 00:31:49 +0545

    restructure application folder structure

---
+++ app/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+
+import { Text, View } from 'react-native';
+
+class Greeting extends Component {
+  render() {
+    return (
+      <Text>Hello {this.props.name}!</Text>
+    );
+  }
+}
+
+const App = () => {
+  return (
+      <View style={{alignItems: 'center'}}>
+        <Greeting name='Rexxar' />
+        <Greeting name='Jaina' />
+        <Greeting name='Valeera' />
+      </View>
+  );
+};
+
+export default App;
